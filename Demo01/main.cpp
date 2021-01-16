2a193a121624a9946bf974a870989abc765c7bfe
liuyunxiaoPC <1614553756@qq.com>
2021-01-16 20:14:57 +0800

    添加项目文件。

---
+++ Demo01/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+template<typename First, typename... Other>
+auto sum(First first, Other... other)
+{
+    if constexpr (sizeof...(Other) > 0)
+    {
+        return first + sum(other...);
+    }
+    else
+    {
+        return first;
+    }
+}
+
+int main()
+{
+    cout << sum(1, 2, 3.5) << endl;
+
+    return 0;
+}
