2003ea4ad42fb3fee1eeb64f5f77a9a0fc39562b
liuyunxiaoPC <1614553756@qq.com>
2021-02-18 00:51:49 +0800

    问题解决，原因：function与lambda返回值不一致

--- Demo01/test32.cpp
+++ Demo01/test32.cpp
@@ -29,25 +29,30 @@ namespace test32
         //    const A& yy = func().v;
         //}
         //cout << "##################" << endl;
-        //{
-        //    A info = 1;
-        //    cout << "1111111111" << endl;
-        //    /*std::function<const A& ()>*/auto func = [&info]() {cout << "return @0x" << &info << endl; return info; };
-        //    cout << "2222222222" << endl;
-        //    func().v;
-        //    cout << "3333333333" << endl;
-        //    info.v = 2;
-        //    cout << "4444444444" << endl;
-        //    func().v;
-        //}
+        {
+            A info = 1;
+            cout << "1111111111" << endl;
+            std::function<const A& ()> func = [&info]()->const A& {cout << "return @0x" << &info << endl; return info; };
+            cout << "2222222222" << endl;
+            func().v;
+            cout << "3333333333" << endl;
+            info.v = 2;
+            cout << "4444444444" << endl;
+            func().v;
+
+            //lambda表达式默认返回值类型
+            auto ff = [&info]() {return info; };
+            using T = decltype(ff()); //T = A
+
+        }
 
         {
             std::string info = "1";
             cout << "info @0x" << &info << endl;
             cout << "1111111111" << endl;
 
-            //TODO 用function接收效果是错的，或者直接崩掉
-            /*std::function<const std::string& ()>*/auto func = [&info]()->const std::string& {
+            //lambda表达式默认返回值类型，但是function返回引用类型，导致了返回一个局部变量的引用
+            std::function<const std::string& ()> func = [&info]()->const std::string& {
                 cout << "return @0x" << &info << endl;
                 return info;
             };
